76b87e09a214730494bee2a10b93c047144b23bd
Abhishek Shukla <abhishek.s.shukla@slrtce.in>
2025-08-25 18:08:07 +0530

    Fixed delete_all and added redirect/url_for

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for  # <-- added redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-
 client = MongoClient("mongodb+srv://Abhishek:Abhi%4012345@student-report.i3gmep4.mongodb.net/?retryWrites=true&w=majority&appName=student-report")
-
 db = client["School"]
 students_collection = db["Students"]
 
@@ -47,6 +45,11 @@ def submit():
 def view_all():
     all_students = list(students_collection.find())
     return render_template("students.html", students=all_students)
+
+@app.route("/delete_all", methods=["POST"])
+def delete_all():
+    students_collection.delete_many({})  # use correct variable
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run(debug=True)
